7681433d6437905de5e699e9445e13fb618691f6
Brycewan <bohuiwan@gmail.com>
2026-03-02 23:12:30 +0800

    [MODEL] Embedding class

--- model/nanollm.py
+++ model/nanollm.py
@@ -31,3 +31,28 @@ class Linear(nn.Module):
         
     def forward(self, x: Tensor) -> Tensor:
         return einops.einsum(x, self.weight, "b ... in, out in -> b ... out")
+    
+    
+class Embedding(nn.Module):
+    def __init__(
+        self,
+        num_embeddings: int,
+        embedding_dim: int,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.weight = nn.Parameter(
+                torch.empty((num_embeddings, embedding_dim), **factory_kwargs)
+            )
+        self.reset_parameters()
+        
+    def reset_parameters(self) -> None:
+        # N(µ = 0, σ2=1)
+        nn.init.trunc_normal_(self.weight, mean=0.0, std=1.0, a=-3.0, b=3.0)
+        
+    def forward(self, token_ids: Tensor) -> Tensor:
+        return self.weight[token_ids]
